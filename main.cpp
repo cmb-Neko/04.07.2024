70b256a4e6d51583641893722fe46d50d456d362
cmb-Neko <h1t0nek0uwu@gmal.com>
2024-07-04 20:01:22 +0300

    lab_1_unfinished

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+struct Rectangle
+{
+private:
+	int *_height;
+	int *_width;
+	int *_marginLeft;
+	int *_marginTop;
+public:
+	Rectangle()
+	{
+		*_height = 1;
+		*_width = 1;
+		*_marginLeft = 0;
+		*_marginTop = 0;
+	}
+	Rectangle(int height)
+};
+
+
+
+
+
+
+
+int main()
+{
+
+}
