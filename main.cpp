e89d1386f197241c45f632f1c879a51c47470d06
cmb-Neko <h1t0nek0uwu@gmal.com>
2024-07-04 20:46:40 +0300

    lab1_unfinished

--- main.cpp
+++ main.cpp
@@ -5,19 +5,29 @@ using namespace std;
 struct Rectangle
 {
 private:
-	int *_height;
-	int *_width;
-	int *_marginLeft;
-	int *_marginTop;
+	int _height;
+	int _width;
+	int _marginLeft;
+	int _marginTop;
+
 public:
 	Rectangle()
 	{
-		*_height = 1;
-		*_width = 1;
-		*_marginLeft = 0;
-		*_marginTop = 0;
+		_height = 1;
+		_width = 1;
+		_marginLeft = 0;
+		_marginTop = 0;
 	}
-	Rectangle(int height)
+	Rectangle(int height) : Rectangle()
+	{
+		SetHeight(height, _height);
+	}
+
+	//Setters 
+	void SetHeight(int height, int _height)
+	{
+		_height = height;
+	}
 };
 
 
@@ -28,5 +38,6 @@ public:
 
 int main()
 {
-
+	Rectangle Hector;
+	Hector;
 }
